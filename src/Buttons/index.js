e757ac3a42ba8f7dd26f9d81cd6b856a4b60a9e8
Maciej Lukaszewski <mlukaszewski86@gmail.com>
2023-01-19 18:19:17 +0100

    change class name and added color to disabled

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,17 +1,17 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     tasks.length > 0 && (
         <div className="buttons">
             <button
                 onClick={toggleHideDone}
-                className={`buttons__button`}
+                className="buttons__button"
             >
                 {hideDone ? "Pokaż" : "Ukryj"} ukończone
             </button>
             <button
-                className={`${!tasks.every(({ done }) => done) ?
-                    "buttons__button" : " buttons__button--disabled"}`}
+                onClick={setAllDone}
+                className="buttons__button"
                 disabled={tasks.every(({ done }) => done)}>
                 Ukończ wszystkie
             </button>
